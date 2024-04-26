cc3906075ed2d0b9a134fd38543784b81670be54
Joel Rodrigues <joel_silva_rodrigues@hotmail.com>
2024-04-26 18:21:54 +0100

    First Commit

---
+++ Infrastructure/Controllers/XboxGamepad.cs
@@ -0,0 +1,128 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Infrastructure.Controllers
+{
+    public class XboxGamepad
+    {
+
+        public delegate void testEventHandler(object source, string message);
+        public event testEventHandler testEvento;
+
+        private int MovementDivider = 2_000;
+        private int _refreshRate = 60;
+
+        //Deadzones
+        private int _leftDeadzone = 200;
+        private int _rightDeadzone = 200;
+
+        private Timer _timer;
+        private Controller _controller;
+
+        private bool _wasADown;
+        private bool _wasBDown;
+
+        public XboxGamepad(int RefreshRate = 60)
+        {
+            _controller = getConnectedController();
+            _timer = new Timer(obj => Update());
+            _refreshRate = RefreshRate;
+        }
+
+        public void Start()
+        {
+            _timer.Change(0, 1000 / _refreshRate);
+        }
+
+        private Controller getConnectedController()
+        {
+            var controllers = new[] { 
+                new Controller(UserIndex.One), 
+                new Controller(UserIndex.Two), 
+                new Controller(UserIndex.Three), 
+                new Controller(UserIndex.Four)
+            };
+            return controllers?.FirstOrDefault(x => x.IsConnected) ?? new Controller(UserIndex.One);
+        }
+
+
+        private void Update()
+        {
+            _controller.GetState(out var state);
+            Movement(state);
+
+            //LeftButton(state);
+            //RightButton(state);
+        }
+
+        private void Movement(State state)
+        {
+            float LX = state.Gamepad.LeftThumbX;
+            float LY = state.Gamepad.LeftThumbY;
+
+            //determine how far the controller is pushed
+            double magnitude = Math.Sqrt(LX * LX + LY * LY);
+
+            //determine the direction the controller is pushed
+            double normalizedLX = LX / magnitude;
+            double normalizedLY = LY / magnitude;
+
+            double normalizedMagnitude = 0;
+
+            //Console.Write("normalizedLX - " + normalizedLX);
+            //Console.Write("normalizedLY - " + normalizedLY);
+
+            //check if the controller is outside a circular dead zone
+            if (magnitude > Gamepad.LeftThumbDeadZone)
+            {
+                //clip the magnitude at its expected maximum value
+                if (magnitude > 32767) magnitude = 32767;
+
+                //adjust magnitude relative to the end of the dead zone
+                magnitude -= Gamepad.LeftThumbDeadZone;
+
+                //optionally normalize the magnitude with respect to its expected range
+                //giving a magnitude value of 0.0 to 1.0
+                normalizedMagnitude = magnitude / (32767 - Gamepad.LeftThumbDeadZone);
+                testEvento(this, normalizedMagnitude.ToString());
+            }
+            else //if the controller is in the deadzone zero out the magnitude
+            {
+                magnitude = 0.0;
+                normalizedMagnitude = 0.0;
+            }
+
+
+
+            var x = state.Gamepad.LeftThumbX / MovementDivider;
+            var y = state.Gamepad.LeftThumbY / MovementDivider;
+            //Console.Write("LEFT X - " + state.Gamepad.LeftThumbX);
+            //Console.Write("LEFT Y - " + state.Gamepad.LeftThumbY);
+            //Console.Write(y);
+            //_mouseSimulator.MoveMouseBy(x, -y);
+            //32768
+        }
+
+        private void RightButton(State state)
+        {
+            var isBDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B);
+            //if (isBDown && !_wasBDown) _mouseSimulator.RightButtonDown();
+            //if (!isBDown && _wasBDown) _mouseSimulator.RightButtonUp();
+            _wasBDown = isBDown;
+        }
+
+        private void LeftButton(State state)
+        {
+            var isADown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A);
+            //if (isADown && !_wasADown) _mouseSimulator.LeftButtonDown();
+            //if (!isADown && _wasADown) _mouseSimulator.LeftButtonUp();
+            _wasADown = isADown;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Device.Pwm;
+using System.Device.Pwm.Drivers;
+using Iot.Device.ServoMotor;
+using TestProject.Infrastructure.Controllers;
+
+
+
+//CONTROLLER
+var inputMonitor = new XboxGamepad();
+inputMonitor.testEvento += onTestAction;
+inputMonitor.Start();
+
+using PwmChannel pwmChannel = PwmChannel.Create(0, 0, 50);
+var spwmChannel = new SoftwarePwmChannel(18, frequency: 50, 0.5,true);
+using ServoMotor servoMotor = new ServoMotor(spwmChannel);
+
+// Samples.
+WritePulseWidth(pwmChannel, servoMotor);
+// WriteAngle(pwmChannel, servoMotor);
+// Methods
+
+static void onTestAction(object source, string message)
+{
+    Console.WriteLine(message);
+}
+
+void WritePulseWidth(PwmChannel pwmChannel, ServoMotor servoMotor)
+{
+    //servoMotor.Calibrate(180, 0.5, 2.5);
+    servoMotor.Start();
+    
+    while (true)
+    {
+        Console.WriteLine("Enter a pulse width in microseconds ('Q' to quit). ");
+        string? pulseWidth = Console.ReadLine();
+
+        if (pulseWidth?.ToUpper() is "Q" or null)
+        {
+            break;
+        }
+
+        if (!int.TryParse(pulseWidth, out int pulseWidthValue))
+        {
+            Console.WriteLine($"Can not parse {pulseWidth}.  Try again.");
+        }
+
+        servoMotor.WritePulseWidth(pulseWidthValue);
+        Console.WriteLine($"Duty Cycle: {pwmChannel.DutyCycle * 100.0}%");
+    }
+
+    servoMotor.Stop();
+}
+
+void WriteAngle(PwmChannel pwmChannel, ServoMotor servoMotor)
+{
+    servoMotor.Start();
+
+    while (true)
+    {
+        Console.WriteLine("Enter an angle ('Q' to quit). ");
+        string? angle = Console.ReadLine();
+
+        if (angle?.ToUpper() is "Q" or null)
+        {
+            break;
+        }
+
+        if (!int.TryParse(angle, out int angleValue))
+        {
+            Console.WriteLine($"Can not parse {angle}.  Try again.");
+        }
+
+        servoMotor.WriteAngle(angleValue);
+        //Console.WriteLine($"Duty Cycle: {pwmChannel.DutyCycle * 100.0}%");
+    }
+
+    servoMotor.Stop();
+}
